b4265ed6509516c9b506f833b264d0d6d150481b
Njoki Kamau <njokifaith61@gmail.com>
2020-07-20 13:35:04 +0300

    added basice routes to display the different templates available

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,5 +20,76 @@ public class App {
             port = 4567;
         }
         port(port);
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "homepage.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/endangered", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "endangeredSpecies.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/ranger-guide", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "userGuide.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/sighting-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "sightingForm.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/sighting-success", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "sightingSuccess.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/sighting-edit", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "sightingEdit.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/sighting-store", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "sightingStore.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/animal-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "animalForm.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/animal-success", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "animalSuccess.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/animal-store", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "animalStore.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/animal-edit", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "animalEdit.hbs");
+        }, new HandlebarsTemplateEngine());
+
+    //animal CREATE and push to success page routing
+
+    //animal CREATE And save to animal store page routing
+
+    //sighting CREATE and push to success  page routing
+
+    //sighting CREATE and save to sighting store page to be reviewed
+
+    //animal Retrieve routing
+
+    //sighting retrieve routing
+
+    //animal update routing
+
+    //sighting update routing
+
+    //animal delete routing
+    // sighting delete routing
+
+
+
+
+
+
     }
+
 }
